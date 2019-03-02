a27d4b016e1272e394430024fb524f4594917224
James_Gu <gujianming@gmail.com>
2019-03-02 21:34:36 +0800

    add ex25.py

---
+++ ex25.py
@@ -0,0 +1,58 @@
+# Exericse 25
+# Even More Pactice
+
+def break_words(stuff):
+    """ This function will break up words for us. """
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """ sorts the words. """
+    return sorted(words)
+
+def print_first_word(words):
+    """ print the first word after popping it off. """
+    word = words.pop(0)
+    print(word)
+
+def print_last_word(words):
+    """ prints the last word after popping it off. """
+    word = words.pop(-1)
+    print(word)
+
+def sort_sentence(sentence):
+    """ takes in a full sentence and returns the sorted words. """
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+
+sentence = "All good things come to those who wait."
+words = break_words(sentence)
+print(words)
+
+sorted_words = sort_words(words)
+print(sorted_words)
+
+print_first_word(words)
+print_last_word(words)
+print(words)
+
+print_first_word(sorted_words)
+print_last_word(sorted_words)
+print(sorted_words)
+
+sorted_words = sort_sentence(sentence)
+print(sorted_words)
+
+print_first_and_last(sentence)
+print_first_and_last_sorted(sentence)
